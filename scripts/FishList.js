6e3beb0c3de69c4f2178a758c165f22af2b123c8
Alex Dudley <alexdudley@me.com>
2021-02-25 16:17:27 -0600

    second push

---
+++ scripts/FishList.js
@@ -0,0 +1,28 @@
+/**
+ *  FishList which renders individual fish objects as HTML
+ */
+
+// TODO: Import `useFish` from the data provider module
+const allTheFish = useFish ()
+
+import { useFish } from "./FishDataProvider.js"
+import { Fish } from "./Fish.js"
+
+Fish()
+
+export const FishList = () => {
+    const fishes = useFish()
+
+    const DOMLocation = document.querySelector("#fishList")
+    
+    // Generate all of the HTML for all of the fish
+
+    let fishHTMLRepresentations = "";
+
+    for (const oneFish of fishes) {
+        fishHTMLRepresentations += Fish(oneFish);
+    }
+console.log("fishHTMLRep", fishHTMLRepresentations);
+    
+DOMLocation.innerHTML = fishHTMLRepresentations;
+}
---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+console.log("too close for missiles, switching to guns.");
+
+
+import { FishList } from './FishList.js'
+
+
+
+
+
+
+
+
+
+// import { useFish } from './FishDataProvider.js'
+
+// const allTheFish = useFish ()
+
+// for (const fish of allTheFish) {
+//     console.log(fish)
+// }
+
+// Import the FishList here
